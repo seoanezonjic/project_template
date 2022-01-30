e9716b562ef025e630db800b7d9b898795ca3270
Sergio Martín <79966117+smv762e@users.noreply.github.com>
2022-01-30 16:47:56 +0100

    Añadido guardado de gráficas a PDF

--- code/Analisis_EG_network.R
+++ code/Analisis_EG_network.R
@@ -29,6 +29,7 @@ sizeGrWindow(9, 5)
 par(mfrow = c(1,2));
 cex1 = 0.9;
 
+pdf(file = 'independenceScale_meanConnectivity.pdf', width = 8,height = 6)
 plot(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
      xlab="Soft Threshold (power)",ylab="Scale Free Topology Model Fit,signed R^2",type="n",
      main = paste("Scale independence"));
@@ -41,12 +42,13 @@ plot(sft$fitIndices[,1], sft$fitIndices[,5],
      xlab="Soft Threshold (power)",ylab="Mean Connectivity", type="n",
      main = paste("Mean connectivity"))
 text(sft$fitIndices[,1], sft$fitIndices[,5], labels=powers, cex=cex1,col="red")
+dev.off()
 
 # Transformación del tipo de valores para todas las columnas (Una función no detecta los valores si no son tipo NUMERIC)
 test <- datExpr
 
 test <- setNames(data.frame(lapply(test, as.numeric)), 
-                        colnames(test))
+                 colnames(test))
 sum(sapply(test, is.numeric))
 
 # Construcción de la red de genes
@@ -64,10 +66,12 @@ table(net$colors)
 sizeGrWindow(12, 9)
 mergedColors = labels2colors(net$colors)
 
+pdf(file = 'clusterDendrogram.pdf', width = 8,height = 6)
 plotDendroAndColors(net$dendrograms[[1]], mergedColors[net$blockGenes[[1]]],
                     "Module colors",
                     dendroLabels = FALSE, hang = 0.03,
                     addGuide = TRUE, guideHang = 0.05)
+dev.off()
 
 # Información de genes y módulos
 moduleLabels = net$colors
